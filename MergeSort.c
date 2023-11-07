c4b078e1d9dde9a70c62e78e2743daf39554b344
Henrique César Higino Holanda Cordeiro <codenomess@hotmail.com>
2023-11-07 14:45:21 -0300

    MergeSort and QuickSort Sample

---
+++ MergeSort.c
@@ -0,0 +1,48 @@
+void merge(long long int arr[], int l, int r)
+{
+    int m = (l + r) / 2;
+    int i = l;
+    int j = m + 1;
+    long long int temp[r+1];
+    for (int n = 0; n<=r; n++)
+    {
+        temp[n] = arr[n];
+    }
+
+    for (int k = l; k<=r; k++)
+    {
+        if (i > m)
+        {
+            arr[k] = temp[j];
+            j++;
+        }
+        else if (j > r)
+        {
+            arr[k] = temp[i];
+            i++;
+        }
+        else if (temp[i] <= temp[j]) //definir maior ou menor para crescente ou decrescente
+        {
+            arr[k] = temp[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = temp[j];
+            j++;
+        }
+    }
+}
+
+void mergesort(long long int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
+
+        merge(arr, l, r);
+    }
+}
---
+++ QuickSort.c
@@ -0,0 +1,39 @@
+int HoarePartition (int arr[], int l, int r)
+{
+    int p = arr[l];
+    int i = l;
+    int j = r+1;
+    do
+    {
+        do
+        {
+            i++;
+        }while(!(arr[i] >= p || i >= r));
+        do
+        {
+            j--;
+        }while(!(arr[j] <= p));
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }while(!(i >= j));
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    temp = arr[l];
+    arr[l] = arr[j];
+    arr[j] = temp;
+    return j;
+}
+
+void quicksort (int arr[], int l, int r)
+{
+    if (l < r)
+    {
+                                            //É possível usar outros tipos de partição
+        int S = HoarePartition(arr, l, r);  //existem métodos de achar um pivô melhor,
+                                            //mas vamos estabelecer o primeiro elemento como pivô
+        quicksort(arr, l, S);
+        quicksort(arr, S+1, r);
+    }
+}
