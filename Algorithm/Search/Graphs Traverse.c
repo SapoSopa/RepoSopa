b5ac43580d258272280967eb939d82972c03e9fd
Sapo Sopa <hchhc@cin.ufpe.br>
2024-02-06 15:12:00 -0300

    ..

--- Algorithm/Search/Graphs Traverse.c
+++ Algorithm/Search/Graphs Traverse.c
@@ -23,11 +23,45 @@ void graphTraverse(Graph *G)                    // Algoritmo genérico de busca;
 // Algortimos de Busca;
 
 // Busca em Profundidade (Depth-First Search [DFS]);
+// Funciona como uma pilha, caso tente armazenar os vértices visitados;
 
 void DFS(Graph *G, int v)
 {
-    // Alguma coisa antes de começar a visitar o vértice (preVisit(G, v));
+    // Alguma coisa antes de visitar o vértice (preVisit(G, v));
     setMark(G, v, 1);                           // Marca o vértice como visitado;
     int w = first(G, v);                        // Pega o primeiro vértice adjacente;
-    while (wgetMark
+    while (w < G->V)
+    {
+        if (!getMark(G, w))
+        {
+            DFS(G, w);                          // Chama a função recursivamente para visitar o vértice;
+        }
+        w = next(G, v, w);                      // Pega o próximo vértice adjacente;
+    }
+    // Alguma coisa depois de visitar o vértice (posVisit(G, v));
+}
+//// Exemplo de uso;
+////// Busca Topológica;
+#include "../List/Array Stack.c" // As funções de "../List/Linked List Stack.c" também funciona nessa Busca;
+// Executar o graphTraverse para limpar os marcadores;
+void toposort(Graph *G, int v, Stack *s)
+{
+    setMark(G, v, 1);                           // Marca o vértice como visitado;
+    int w = first(G, v);                        // Pega o primeiro vértice adjacente;
+    while (w < G->V)
+    {
+        if (!getMark(G, w))
+        {
+            toposort(G, w, s);                  // Chama a função recursivamente para visitar o vértice;
+        }
+        w = next(G, v, w);                      // Pega o próximo vértice adjacente;
+    }
+    push(s, v);                                 // Adiciona o vértice na pilha;
+}
+
+// Busca em Largura (Breadth-First Search [BFS]);
+
+void BFS(Graph *G, int v)
+{
+
 }
